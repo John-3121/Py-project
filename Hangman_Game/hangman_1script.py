f788f6218677f2b875ba0a2f904f8657cda81ce5
John-3121 <johncarlocabadsan@gmail.com>
2024-11-11 09:42:57 +0800

    Update hangman_1script.py

--- Hangman_Game/hangman_1script.py
+++ Hangman_Game/hangman_1script.py
@@ -1,6 +1,6 @@
 import random 
 import string
-words = ["dog","cabinet","python","sky","gold","racoon","headphone","shirt"#You can change this or add more
+words = ["dog","cabinet","python","sky","gold","racoon","headphone","shirt"]#You can change this or add more
 
 lives_visual_dict = {
         0: """
